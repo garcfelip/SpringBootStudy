4c28c4acd7d04a4e8091d0afedefffb58cff39d0
fmgarcia <fmgarcia@viptech.com.br>
2023-03-16 08:53:43 -0300

    Spring Boot Initializr

---
+++ src/test/java/br/com/viptech/springboot/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.viptech.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
